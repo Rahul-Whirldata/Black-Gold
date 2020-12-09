563c0dc519e54ab33c1e5b495bac4ce3a0ed4ba6
Rahul-Whirldata <rchoudhary@whirldatascience.com>
2020-12-09 22:05:07 +0530

    forward curves page completed

--- test_COApp/forwardCurvesPage.xaml.cs
+++ test_COApp/forwardCurvesPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Syncfusion.SfChart.XForms;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -16,7 +17,7 @@ namespace test_COApp
     public class GraphModel
     {
         public string category { get; set; }
-        public double settle { get; set; }
+        public double settle0 { get; set; }
         public double settle1 { get; set; }
         public string settle1_desc { get; set; }
         public double settle2 { set; get; }
@@ -37,7 +38,7 @@ namespace test_COApp
     {
         public string base_date { get; set; }
         public string lookForward { get; set; }
-        public List<string> compare_dates { get; set; }
+        public ObservableCollection<string> compare_dates { get; set; }
         public string is_settle { get; set; }
         public string monthOf { get; set; }
         public string type { get; set; }
@@ -50,11 +51,13 @@ namespace test_COApp
         {
             var values = inputjson;
             var client = new HttpClient();
-            client.BaseAddress = new Uri("http://192.168.1.169:5000");
+            client.BaseAddress = new Uri("http://192.168.3.120:5000");
 
             string jsondata = JsonConvert.SerializeObject(values);
             var content = new StringContent(jsondata, Encoding.UTF8, "application/json");
 
+            Debug.WriteLine("content before sending api call -----> "+content);
+            
             HttpResponseMessage response = await client.PostAsync("/test", content);
 
             var json = await response.Content.ReadAsStringAsync();
@@ -62,24 +65,101 @@ namespace test_COApp
             string transformedJson = @"{'GraphView':" + json + "}";
 
             var jsonDataCollection = JsonConvert.DeserializeObject<GraphViewModel>(transformedJson);
+            Debug.WriteLine("jsondatacollection category ----> ");
+            foreach (var i in jsonDataCollection.GraphView)
+            {
+                Debug.WriteLine(i.category);
+                
+            }
+
             if (Id == 1)
             {
-                chart1.ItemsSource = jsonDataCollection.GraphView;
-            } else if (Id == 2)
+                mainChart1.Series.Clear();
+                foreach (var i in compareDateList)
+                {
+                    Debug.WriteLine("id 1 Compare list " + i);
+                }
+
+                for (var i = 0; i <= compareDateList.Count; i++)
+                {
+                    LineSeries chart1 = new LineSeries();
+                    chart1.EnableAnimation = true;
+                    chart1.EnableDataPointSelection = true;
+                    chart1.EnableTooltip = true;
+                    chart1.ItemsSource = jsonDataCollection.GraphView;
+                    chart1.XBindingPath = "category";
+                    chart1.YBindingPath = "settle"+i.ToString();
+
+                    mainChart1.Series.Add(chart1);
+                    Debug.WriteLine("id1 settle -----> " + "settle"+i.ToString());
+                }
+                
+                
+            } 
+            else if (Id == 2)
             {
-                chart2.ItemsSource = jsonDataCollection.GraphView;
-            } else if (Id == 3)
+                mainChart2.Series.Clear();
+                foreach (var i in compareDateList)
+                {
+                    Debug.WriteLine("id 1 Compare list " + i);
+                }
+                for (var i = 0; i <= compareDateList.Count; i++)
+                {
+                    LineSeries chart2 = new LineSeries();
+                    chart2.EnableAnimation = true;
+                    chart2.EnableDataPointSelection = true;
+                    chart2.EnableTooltip = true;
+                    chart2.ItemsSource = jsonDataCollection.GraphView;
+                    chart2.XBindingPath = "category";
+                    chart2.YBindingPath = "settle"+i.ToString();
+
+                    mainChart2.Series.Add(chart2);
+                    Debug.WriteLine("id2 settle ------> "+"settle"+i.ToString());
+                }          
+            } 
+            else if (Id == 3)
             {
-                chart1.ItemsSource = jsonDataCollection.GraphView;
-                chart2.ItemsSource = jsonDataCollection.GraphView;
+                mainChart1.Series.Clear();
+                mainChart2.Series.Clear();
+                for (var i = 0; i <= compareDateList.Count; i++)
+                {
+                    LineSeries chart1 = new LineSeries();
+                    chart1.EnableAnimation = true;
+                    chart1.EnableDataPointSelection = true;
+                    chart1.EnableTooltip = true;
+                    chart1.ItemsSource = jsonDataCollection.GraphView;
+                    chart1.XBindingPath = "category";
+                    chart1.YBindingPath = "settle"+i.ToString();
+
+                    mainChart1.Series.Add(chart1);
+
+                    LineSeries chart2 = new LineSeries();
+                    chart2.EnableAnimation = true;
+                    chart2.EnableDataPointSelection = true;
+                    chart2.EnableTooltip = true;
+                    chart2.ItemsSource = jsonDataCollection.GraphView;
+                    chart2.XBindingPath = "category";
+                    chart2.YBindingPath = "settle"+i.ToString();
+
+                    mainChart2.Series.Add(chart2);
+
+                    Debug.WriteLine("id3 settle -----> "+"settle"+i.ToString());
+                }
+                    
             }
+            mainChart1.Legend = new ChartLegend() { OverflowMode = ChartLegendOverflowMode.Wrap };
+            mainChart1.Legend.LabelStyle.TextColor = Color.Black;
+            mainChart2.Legend = new ChartLegend() { OverflowMode = ChartLegendOverflowMode.Wrap };
+            mainChart2.Legend.LabelStyle.TextColor = Color.Black;
         }
 
         public forwardCurvesPage()
         {
+            Debug.WriteLine("inside constructor before initialize component");
             InitializeComponent();
+            Debug.WriteLine("inside constructor after initialize component");
             base_date.Date = new DateTime(2020, 10, 8);
-
+            //compare_dates.Date = new DateTime(2020, 10, 7);
             var pick1 = new List<string>();
             pick1.Add("Outright");
             pick1.Add("Spread");
@@ -111,6 +191,11 @@ namespace test_COApp
             pickerNum2.SelectedIndex = 0;
 
             compareDateListView.ItemsSource = compareDateList;
+            Debug.WriteLine("Constructor comparedatelist ------> ");
+            foreach (var i in compareDateList)
+            {
+                Debug.WriteLine(i);
+            }
         }
 
         private void base_date_DateChanged(object sender, DateChangedEventArgs e)
@@ -137,13 +222,14 @@ namespace test_COApp
                 {
                     base_date = baseDate,
                     lookForward = look_forward,
-                    compare_dates = new List<string>() { },
+                    compare_dates = compareDateList,
                     is_settle = "true",
                     monthOf = pickerNum1.SelectedItem.ToString(),
                     type = pickType
                 };
 
                 DataProvider(val, 1);
+                Debug.WriteLine("base date first try statement ran !!!!!");
             }
             catch (NullReferenceException r)
             {
@@ -176,13 +262,14 @@ namespace test_COApp
                 {
                     base_date = baseDate,
                     lookForward = look_forward,
-                    compare_dates = new List<string>() { },
+                    compare_dates = compareDateList,
                     is_settle = "true",
                     monthOf = pickerNum2.SelectedItem.ToString(),
                     type = pickType
                 };
 
                 DataProvider(val, 2);
+                Debug.WriteLine("base date 2nd try statement ran !!!!!!!");
             }
             catch (NullReferenceException r)
             {
@@ -219,12 +306,13 @@ namespace test_COApp
                 {
                     base_date = baseDate,
                     lookForward = look_forward,
-                    compare_dates = new List<string>() { },
+                    compare_dates = compareDateList,
                     is_settle = "true",
                     monthOf = pickerNum1.SelectedItem.ToString(),
                     type = pickType
                 };
                 DataProvider(val, 1);
+                Debug.WriteLine("look forward 1st try statement ran!!!!");
             }
             catch (NullReferenceException r)
             {
@@ -257,12 +345,13 @@ namespace test_COApp
                 {
                     base_date = baseDate,
                     lookForward = look_forward,
-                    compare_dates = new List<string>() { },
+                    compare_dates = compareDateList,
                     is_settle = "true",
                     monthOf = pickerNum2.SelectedItem.ToString(),
                     type = pickType
                 };
                 DataProvider(val, 2);
+                Debug.WriteLine("look forward second try statement ran !!!!!");
             }
             catch (NullReferenceException r)
             {
@@ -296,12 +385,13 @@ namespace test_COApp
                 {
                     base_date = baseDate,
                     lookForward = look_forward,
-                    compare_dates = new List<string>() { },
+                    compare_dates = compareDateList,
                     is_settle = "true",
                     monthOf = pickerNum1.SelectedItem.ToString(),
                     type = pickType
                 };
                 DataProvider(val, 1);
+                Debug.WriteLine("picker 1 try statement ran !!!!!!!!!!!!!!!!");
             }
             catch (NullReferenceException r)
             {
@@ -337,12 +427,13 @@ namespace test_COApp
                 {
                     base_date = baseDate,
                     lookForward = look_forward,
-                    compare_dates = new List<string>() { },
+                    compare_dates = compareDateList,
                     is_settle = "true",
                     monthOf = pickerNum2.SelectedItem.ToString(),
                     type = pickType
                 };
                 DataProvider(val, 2);
+                Debug.WriteLine("picker 2 try statement ran!!!!!!!!!!");
             }
             catch (NullReferenceException r)
             {
@@ -357,23 +448,205 @@ namespace test_COApp
         private void compareDateListView_ItemTapped(object sender, ItemTappedEventArgs e)
         {
             compareDateList.Remove(e.Item.ToString());
+
+            try
+            {
+                var baseDate = base_date.Date.ToString("yyyy-MM-dd");
+                var look_forward = LookForward.Value.ToString();
+                string pickType = null;
+                if (picker1.SelectedIndex == 0)
+                {
+                    pickType = "O";
+                }
+                else if (picker1.SelectedIndex == 1)
+                {
+                    pickType = "S";
+                }
+                else if (picker1.SelectedIndex == 2)
+                {
+                    pickType = "F";
+                }
+
+                var val = new json()
+                {
+                    base_date = baseDate,
+                    lookForward = look_forward,
+                    compare_dates = compareDateList,
+                    is_settle = "true",
+                    monthOf = pickerNum1.SelectedItem.ToString(),
+                    type = pickType
+                };
+
+                DataProvider(val, 1);
+                Debug.WriteLine("item tapped first try statement ran !!!!!");
+            }
+            catch (NullReferenceException r)
+            {
+                Debug.WriteLine(r);
+            }
+            finally
+            {
+                Debug.WriteLine("executed without error! - item tapped 1");
+            }
+
+            try
+            {
+                var baseDate = base_date.Date.ToString("yyyy-MM-dd");
+                var look_forward = LookForward.Value.ToString();
+                string pickType = null;
+                if (picker2.SelectedIndex == 0)
+                {
+                    pickType = "O";
+                }
+                else if (picker2.SelectedIndex == 1)
+                {
+                    pickType = "S";
+                }
+                else if (picker2.SelectedIndex == 2)
+                {
+                    pickType = "F";
+                }
+
+                var val = new json()
+                {
+                    base_date = baseDate,
+                    lookForward = look_forward,
+                    compare_dates = compareDateList,
+                    is_settle = "true",
+                    monthOf = pickerNum2.SelectedItem.ToString(),
+                    type = pickType
+                };
+
+                DataProvider(val, 2);
+                Debug.WriteLine("item tapped 2nd try statement ran !!!!!!!");
+            }
+            catch (NullReferenceException r)
+            {
+                Debug.WriteLine(r);
+            }
+            finally
+            {
+                Debug.WriteLine("executed without error! - item tapped 2");
+            }
+
             if (compareDateList.Count < 5)
             {
                 compare_dates.IsEnabled = true;
+                //Debug.WriteLine("Inside item tapped if statement...comparelist count -----> "+ compareDateList.Count);
+                //Debug.WriteLine("inside item tapped if statment ... comparelist");
+                //foreach (var i in compareDateList)
+                //{
+                //    Debug.WriteLine(i);
+                //}
             }
+            //Debug.WriteLine("outside item tapped if statment ... comparelist");
+            //foreach (var i in compareDateList)
+            //{
+            //    Debug.WriteLine(i);
+            //}
             
         }
 
         private void compare_dates_DateSelected(object sender, DateChangedEventArgs e)
         {
-            if (compareDateList.Count < 5)
+            compareDateList.Add(e.NewDate.ToString("yyyy-MM-dd"));
+
+            try
             {
-                compareDateList.Add(compare_dates.Date.ToString("yyyy-MM-dd"));
+                var baseDate = base_date.Date.ToString("yyyy-MM-dd");
+                var look_forward = LookForward.Value.ToString();
+                string pickType = null;
+                if (picker1.SelectedIndex == 0)
+                {
+                    pickType = "O";
+                }
+                else if (picker1.SelectedIndex == 1)
+                {
+                    pickType = "S";
+                }
+                else if (picker1.SelectedIndex == 2)
+                {
+                    pickType = "F";
+                }
+
+                var val = new json()
+                {
+                    base_date = baseDate,
+                    lookForward = look_forward,
+                    compare_dates = compareDateList,
+                    is_settle = "true",
+                    monthOf = pickerNum1.SelectedItem.ToString(),
+                    type = pickType
+                };
+
+                DataProvider(val, 1);
+                Debug.WriteLine("compare date first try statement ran !!!!!");
             }
-            else if (compareDateList.Count < 5)
+            catch (NullReferenceException r)
+            {
+                Debug.WriteLine(r);
+            }
+            finally
+            {
+                Debug.WriteLine("executed without error! - compare date change 1");
+            }
+
+            try
+            {
+                var baseDate = base_date.Date.ToString("yyyy-MM-dd");
+                var look_forward = LookForward.Value.ToString();
+                string pickType = null;
+                if (picker2.SelectedIndex == 0)
+                {
+                    pickType = "O";
+                }
+                else if (picker2.SelectedIndex == 1)
+                {
+                    pickType = "S";
+                }
+                else if (picker2.SelectedIndex == 2)
+                {
+                    pickType = "F";
+                }
+
+                var val = new json()
+                {
+                    base_date = baseDate,
+                    lookForward = look_forward,
+                    compare_dates = compareDateList,
+                    is_settle = "true",
+                    monthOf = pickerNum2.SelectedItem.ToString(),
+                    type = pickType
+                };
+
+                DataProvider(val, 2);
+                Debug.WriteLine("compare date 2nd try statement ran !!!!!!!");
+            }
+            catch (NullReferenceException r)
+            {
+                Debug.WriteLine(r);
+            }
+            finally
+            {
+                Debug.WriteLine("executed without error! - compare date change 2");
+            }
+            //Debug.WriteLine("outside compare date changed if statement...comparelist count -----> " + compareDateList.Count);
+            //Debug.WriteLine("outside compare date changed if statment ... comparelist");
+            //foreach (var i in compareDateList)
+            //{
+            //    Debug.WriteLine(i);
+            //}
+            if (compareDateList.Count == 5)
             {
                 compare_dates.IsEnabled = false;
+                //Debug.WriteLine("insode compare date changed if statement...comparelist count -----> " + compareDateList.Count);
+                //Debug.WriteLine("inside compare date changed if statment ... comparelist");
+                //foreach (var i in compareDateList)
+                //{
+                //    Debug.WriteLine(i);
+                //}
             }
+            
         }
     }
 }
